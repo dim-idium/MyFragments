c50baa9b43cb6c2e60601a984da3c9bb8e48d2f2
dimidium <dim.khusnutdin@gmail.com>
2021-08-05 14:39:22 +0500

    second fragment

---
+++ app/src/main/java/com/dimidium/myfrafments/CoatsFragment.java
@@ -0,0 +1,39 @@
+package com.dimidium.myfrafments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CoatsFragment extends Fragment {
+
+    public CoatsFragment() {
+
+
+    }
+
+    public static CoatsFragment newInstance() {
+        CoatsFragment fragment = new CoatsFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_coats, container, false);
+    }
+}
---
+++ app/src/main/java/com/dimidium/myfrafments/CostActivity.java
@@ -0,0 +1,16 @@
+package com.dimidium.myfrafments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cost);
+    }
+
+
+}
